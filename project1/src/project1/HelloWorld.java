c8f8a7b8def60f8f5f2edb5770d3b2fa44d7da34
전태경 <xorud1544@naver.com>
2017-11-02 18:00:55 +0900

    병합 테스트

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("Time is 5:57PM");
+		System.out.println("finished");
 	}
 
 }
